15461d3764838406fc4fe93ee4f6a6a70c9e4533
josephjensen <joe@cryptonumerics.com>
2022-11-08 23:39:42 -0800

    update maxage to allow for 10 minute sessions

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {maxAge: 60000},
+  cookie: {maxAge: 60000 * 10},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
